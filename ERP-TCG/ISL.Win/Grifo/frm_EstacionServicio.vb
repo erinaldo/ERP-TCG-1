fb507def04b94836c68dc9808259ded03a5e748b
Erick Nevado <eeneba@hotmail.com>
2019-10-16 23:21:30 -0500

    20191016-001

---
+++ ERP-TCG/ISL.Win/Grifo/frm_EstacionServicio.vb
@@ -0,0 +1,49 @@
+﻿Public Class frm_EstacionServicio
+    Inherits ISL.Win.frm_MenuPadre
+
+    Public Sub New()
+        ' Llamada necesaria para el Diseñador de Windows Forms.
+        InitializeComponent()
+        ' Agregue cualquier inicialización después de la llamada a InitializeComponent().
+
+    End Sub
+
+#Region "Instancia"
+
+    Private Shared instancia As frm_EstacionServicio = Nothing
+    Private Shared Operacion As String
+
+    Public Overrides Function getInstancia() As frm_MenuPadre
+        If instancia Is Nothing Then
+            Operacion = "Inicializa"
+            instancia = New frm_EstacionServicio()
+        End If
+        instancia.Activate()
+        Return instancia
+    End Function
+
+#End Region
+
+#Region "Declaracion de Variables"
+
+
+
+#End Region
+
+#Region "Eventos"
+
+    Private Sub frm_EstacionServicio_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub frm_EstacionServicio_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
+        instancia = Nothing
+    End Sub
+
+    Private Sub frm_EstacionServicio_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+
+    End Sub
+
+#End Region
+
+End Class
