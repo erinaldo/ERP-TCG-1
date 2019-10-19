ebb627584bee1afd3aa05e9e7dc7bd9638627a1c
usuario <usuario@LAPWILLY>
2019-10-19 10:20:36 -0500

    INTALADOR

--- ERP-TCG/ISL.AccesoDatos/General/d_Conexion.vb
+++ ERP-TCG/ISL.AccesoDatos/General/d_Conexion.vb
@@ -21,7 +21,7 @@ Public Class d_Conexion
     Public Shared Function CadenaConexionSGI() As String
         Try
             'Return ConfigurationManager.ConnectionStrings("ISL").ConnectionString '@0001
-            Return "Data Source=190.117.126.71;Initial Catalog=ERPTCG; Persist Security Info=True; User ID=sa; Password=2450L0rayL0ra; Connect Timeout=120"
+            Return "Data Source=192.168.0.8;Initial Catalog=ERPTCG; Persist Security Info=True; User ID=sa; Password=2450L0rayL0ra; Connect Timeout=120"
             'Dim config As System.Configuration.Configuration = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None)
 
             'Dim TipoConex As String = config.AppSettings.Settings.Item("0").Value
