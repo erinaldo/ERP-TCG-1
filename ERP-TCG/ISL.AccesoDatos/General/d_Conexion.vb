b46a6cfe076048fdec8cb2c5d424da03209fea65
usuario <usuario@LAPWILLY>
2020-01-22 21:05:07 -0500

    Conexion

--- ERP-TCG/ISL.AccesoDatos/General/d_Conexion.vb
+++ ERP-TCG/ISL.AccesoDatos/General/d_Conexion.vb
@@ -27,6 +27,7 @@ Public Class d_Conexion
 
             'DESARROLLO
             Return "Data Source=190.117.126.71;Initial Catalog=ERPTCG0114; Persist Security Info=True; User ID=sa; Password=2450L0rayL0ra; Connect Timeout=120"
+            'Return "Data Source=190.117.126.71;Initial Catalog=ERPTCG0114; Persist Security Info=True; User ID=sa; Password=2450L0rayL0ra; Connect Timeout=120"
 
             'PRODUCCION LOCAL
             'Return "Data Source=LADERA;Initial Catalog=ERPTCG; Persist Security Info=True; User ID=sa; Password=2450L0rayL0ra; Connect Timeout=120"
