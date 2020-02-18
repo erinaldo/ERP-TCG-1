67fc2ec1d55066a833072349ae04707eeafa5276
usuario <usuario@LAPWILLY>
2020-02-18 14:02:49 -0500

    CONEXION

--- ERP-TCG/ISL.AccesoDatos/General/d_Conexion.vb
+++ ERP-TCG/ISL.AccesoDatos/General/d_Conexion.vb
@@ -21,7 +21,6 @@ Public Class d_Conexion
     Public Shared Function CadenaConexionSGI() As String
         Try
             'Return ConfigurationManager.ConnectionStrings("ISL").ConnectionString '@0001
-
             'PRODUCCION INTERNET
             'Return "Data Source=190.117.126.71;Initial Catalog=ERPTCG; Persist Security Info=True; User ID=ERP; Password=Sistem@s789; Connect Timeout=120"
 
@@ -38,7 +37,6 @@ Public Class d_Conexion
             'Return "Data Source=127.0.0.1;Initial Catalog=ERPTCG; Persist Security Info=True; User ID=sa; Password=sql2014; Connect Timeout=120"
 
             'Dim config As System.Configuration.Configuration = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None)
-
             'Dim TipoConex As String = config.AppSettings.Settings.Item("0").Value
             '= ConfigurationManager.AppSettings.Get("TipoConex")
 
