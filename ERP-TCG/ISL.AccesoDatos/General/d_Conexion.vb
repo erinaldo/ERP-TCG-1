000aa36148622c76665ab359b2621522fafe448d
usuario <usuario@LAPWILLY>
2020-03-04 14:47:24 -0500

    Cone

--- ERP-TCG/ISL.AccesoDatos/General/d_Conexion.vb
+++ ERP-TCG/ISL.AccesoDatos/General/d_Conexion.vb
@@ -35,7 +35,7 @@ Public Class d_Conexion
             'Return "Data Source=LADERA;Initial Catalog = ERPTCG1802; Persist Security Info=True; User ID=sa; Password=2450L0rayL0ra; Connect Timeout=120"
 
             'OTROS
-            'Return "Data Source=127.0.0.1;Initial Catalog=ERPTCG; Persist Security Info=True; User ID=sa; Password=sql2014; Connect Timeout=120"
+            'Return "Data Source=127.0.0.1;Initial Catalog=ERPTCG0304; Persist Security Info=True; User ID=sa; Password=sql2014; Connect Timeout=120"
 
             'Dim config As System.Configuration.Configuration = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None)
             'Dim TipoConex As String = config.AppSettings.Settings.Item("0").Value
