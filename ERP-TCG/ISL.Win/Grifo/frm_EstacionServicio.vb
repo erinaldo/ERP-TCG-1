b35792ca1e434b25e8f40a8e18ee621fb0a943cd
usuario <usuario@LAPWILLY>
2020-03-12 12:30:57 -0500

    cambios

--- ERP-TCG/ISL.Win/Grifo/frm_EstacionServicio.vb
+++ ERP-TCG/ISL.Win/Grifo/frm_EstacionServicio.vb
@@ -294,7 +294,7 @@ Public Class frm_EstacionServicio
                     ItemDocumento.IdAlmacen = ItemVenta.IdAlmacen
                     ItemDocumento.IndGravado = ItemVenta.IndImpuesto
                     ItemDocumento.IndServicioMaterial = "M"
-                    ItemDocumento.Cantidad = Math.Round(ItemVenta.Cantidad, DEC, MidpointRounding.AwayFromZero)
+                    ItemDocumento.Cantidad = ItemVenta.Cantidad '@0001
                     ItemDocumento.Precio = Math.Round(ItemVenta.PrecioUnitario, DEC, MidpointRounding.AwayFromZero)
                     ItemDocumento.Subtotal = Math.Round(IIf(ItemVenta.IndImpuesto, ItemVenta.PrecioTotal / (1 + mdblIGV), ItemVenta.PrecioTotal), DEC, MidpointRounding.AwayFromZero)
                     ItemDocumento.Total = Math.Round(ItemVenta.PrecioTotal, DEC, MidpointRounding.AwayFromZero)
